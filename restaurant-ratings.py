ec57ad1f0fa1c1252efb46c3ea510ea7f451c468
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:41:21 -0700

    add completed restaurant ratings program - not in a function

---
+++ restaurant-ratings.py
@@ -0,0 +1,31 @@
+""" Restaurant ratings program
+
+restaurant-ratings.py opens and reads a file, and spits out ratings
+alphabetically by restaurant.
+
+all_restaurant_reviews - the whole dictionary of reviews
+restaurant_review - a single review
+restaurant - key for the review
+"""
+
+input_file = open("scores.txt")
+all_restaurant_reviews = {}
+#sorted_all_restaurant_reviews = []
+
+for line in input_file:
+    stripped_line = line.rstrip()
+    restaurant_review = stripped_line.split(":")
+
+# put each pair in dictionary then alphabetize
+    all_restaurant_reviews[restaurant_review[0]]= restaurant_review[1]
+
+#alphabetize - sorts by default on the keys, sorted returns a list
+#print sorted(all_restaurant_reviews)
+sorted_all_restaurant_reviews = sorted(all_restaurant_reviews)
+
+#printed sorted list from dictionary of restaurants and reviews
+for restaurant_sorted in sorted_all_restaurant_reviews:
+    print "{} is rated at {}.".format(restaurant_sorted, 
+        all_restaurant_reviews[restaurant_sorted])
+
+input_file.close()
